aa2ab10f52e0fdd71b94b6927e2556ebd3c2031f
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-06-03 16:53:57 +0000

    f

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -14,21 +14,21 @@ import { env } from "~/env";
 export const metadata: Metadata = {
   metadataBase: new URL(
     env.VERCEL_ENV === "production"
-      ? "https://turbo.t3.gg"
+      ? "https://elafda.fun"
       : "http://localhost:3000",
   ),
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
+  title: "ELafda",
+  description: "Simple directory for lafdas",
   openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
+    title: "ELafda",
+    description: "Simple directory for lafdas",
+    url: "https://elafda.fun",
+    siteName: "ELafda",
   },
   twitter: {
     card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
+    site: "@icantcodefyi",
+    creator: "@icantcodefyi",
   },
 };
 
